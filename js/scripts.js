42bc52e181e3757939ece04115514a11c0b7e4a1
Aaron Baumgarner <aaron.baumgarner@eagles.ewu.edu>
2015-10-27 15:05:12 -0700

    More Jquery fun

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 /**
  * Created by Aaron on 10/27/2015.
  */
+$('.pattern-1').mouseover(function() {
+   $('body').addClass('pattern-1-bg')
+});
+$('.pattern-1').mouseout(function() {
+    $('body').removeClass('pattern-1-bg')
+});
+$('.pattern-2').mouseover(function() {
+    $('body').addClass('pattern-2-bg')
+});
+$('.pattern-2').mouseout(function() {
+    $('body').removeClass('pattern-2-bg')
+});
+$('.pattern-3').mouseover(function() {
+    $('body').addClass('pattern-3-bg')
+});
+$('.pattern-3').mouseout(function() {
+    $('body').removeClass('pattern-3-bg')
+});
