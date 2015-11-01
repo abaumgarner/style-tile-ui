bf406fe0d819de36d61cf2830a0a96278c8c6850
Aaron Baumgarner <aaron.baumgarner@eagles.ewu.edu>
2015-10-31 17:56:24 -0700

    JavaScript Fun
    Added a toggle feature to the JavaScript and made it slightly more user
    friendly

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
 /**
  * Created by Aaron on 10/31/2015.
  */
-$('.pattern-1').mouseover(function() {
-    $('body').addClass('pat-1');
-});
-$('.pattern-1').mouseout(function() {
-    $('body').removeClass('pat-1');
+var ctr1 = 0;
+$('.pattern-1').click(function() {
+    if(ctr1 === 0){
+        $('body').addClass('pat-1');
+        $('.pattern-1').addClass('pattern-hightlight');
+        ctr1++;
+    }else {
+        $('body').removeClass('pat-1');
+        $('.pattern-1').removeClass('pattern-hightlight');
+        ctr1--;
+    }
 });
 
-$('.pattern-2').mouseover(function() {
-    $('body').addClass('pat-2');
-});
-$('.pattern-2').mouseout(function() {
-    $('body').removeClass('pat-2');
+var ctr2 = 0;
+$('.pattern-2').click(function() {
+    if(ctr2 === 0){
+        $('body').addClass('pat-2');
+        $('.pattern-2').addClass('pattern-hightlight');
+        ctr2++;
+    }else {
+        $('body').removeClass('pat-2');
+        $('.pattern-2').removeClass('pattern-hightlight');
+        ctr2--;
+    }
 });
 
-$('.pattern-3').mouseover(function() {
-    $('body').addClass('pat-3');
-});
-$('.pattern-3').mouseout(function() {
-    $('body').removeClass('pat-3');
+var ctr3 = 0;
+$('.pattern-3').click(function() {
+    if(ctr3 === 0){
+        $('body').addClass('pat-3');
+        $('.pattern-3').addClass('pattern-hightlight');
+        ctr3++;
+    }else {
+        $('body').removeClass('pat-3');
+        $('.pattern-3').removeClass('pattern-hightlight');
+        ctr3--;
+    }
 });
