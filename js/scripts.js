4c7d46fc9aa7e98073874a0650b69b882468dcfe
Aaron Baumgarner <aaron.baumgarner@eagles.ewu.edu>
2015-10-31 17:32:09 -0700

    JavaScript
    Added JavaScript so you can preview the patterns for fun

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Aaron on 10/31/2015.
+ */
+$('.pattern-1').mouseover(function() {
+    $('body').addClass('pat-1');
+});
+$('.pattern-1').mouseout(function() {
+    $('body').removeClass('pat-1');
+});
+
+$('.pattern-2').mouseover(function() {
+    $('body').addClass('pat-2');
+});
+$('.pattern-2').mouseout(function() {
+    $('body').removeClass('pat-2');
+});
+
+$('.pattern-3').mouseover(function() {
+    $('body').addClass('pat-3');
+});
+$('.pattern-3').mouseout(function() {
+    $('body').removeClass('pat-3');
+});
